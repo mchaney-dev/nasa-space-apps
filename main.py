cefef73342e25ab3132f6521eff78fa0837bd956
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2025-10-04 10:56:04 -0400

    implement main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-# entry point
+from fastapi import FastAPI
+from routes import datasets, labels, tiles
 
+app = FastAPI(title="NASA Space Apps 2025", version="0.1.0")
+
+# routers
+app.include_router(datasets.router)
+app.include_router(labels.router)
+app.include_router(tiles.router)
+
+# root route
+@app.get("/")
+def root():
+    return {"message": "Hello world!"}
