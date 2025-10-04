0f2e101840155ddcdea0201d1bbbed0545e9b7f3
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2025-10-04 15:20:19 -0400

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes import datasets, labels, tiles
 
 app = FastAPI(title="NASA Space Apps 2025", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # routers
 app.include_router(datasets.router)
