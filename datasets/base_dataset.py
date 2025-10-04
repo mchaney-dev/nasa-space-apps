b43ad854431c24321ec0c7935366e3a2416b0f0d
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2025-10-04 10:50:20 -0400

    add base dataset implementation

--- datasets/base_dataset.py
+++ datasets/base_dataset.py
@@ -1,1 +1,21 @@
-# base dataset
+from typing import Optional
+
+"""
+Base class for datasets.
+"""
+class BaseDataset:
+    def __init__(self, id: str, name: str, type: str, source: str, description: Optional[str] = None):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.source = source
+        self.description = description
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "source": self.source,
+            "description": self.description
+        }
