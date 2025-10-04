e9267fa9a1a50c76a22670d66888a1c0ccd0b342
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2025-10-04 10:48:55 -0400

    add logging utils

--- core/utils.py
+++ core/utils.py
@@ -1,2 +1,9 @@
-# utility
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+def info(message: str):
+    logging.info(f"[INFO] {message}")
+
+def error(message: str):
+    logging.error(f"[ERROR] {message}")
